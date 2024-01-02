bda5ab4884451165203b92090f32faa61f36b80d
MD. IBRAHIM KHALIL <58024873+ibrahimk4111@users.noreply.github.com>
2024-01-02 13:12:16 +0000

    services fixs

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -11,9 +11,9 @@ const Service = ({ service, index }) => {
   return (
     <Link to={`/detailed_service/${service.id}`} onClick={setDirectoryFalse}>
       <div className='flex flex-col justify-center items-center py-3'>
-        <div className="cursor-pointer hover:bg-bg-card hover:text-white rounded-md transition duration-500 ease-in hover:scale-105 shadow-xl">
-          <div className='h-52 overflow-hidden flex justify-center items-center'>
-            <img className="object-cover h-52 w-full" src={`https://pioneer.kodbel.com${service.img}`} alt="...." />
+        <div className="cursor-pointer hover:bg-bg-card hover:text-white rounded-md transition duration-300 ease-in shadow shadow-slate-400">
+          <div className='overflow-hidden flex justify-center items-center rounded-t-md'>
+            <img className="object-cover h-auto w-auto max-h-40 max-w-full" src={`https://pioneer.kodbel.com${service.img}`} alt="...." />
           </div>
           <div className="p-5">
             <h2 className=" text-lg md:text-xl font-bold mb-2">{service.title}</h2>
